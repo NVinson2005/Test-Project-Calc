4234d00e2d18285e431ccf494b7901e126593c9e
Nicholas Vinson <104808630+NVinson2005@users.noreply.github.com>
2024-06-06 21:12:40 -0500

    intial stages

---
+++ testcalcs.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class testcalcs{
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Would you like to calculate[Y or N]");
+        String uses = scan.nextLine().toUpperCase();
+    }
+}
